313ecf2158847353f9fdce6f513a0e3c1063d750
Advait Vedant <advait.suraj.vedant@gmail.com>
2023-11-15 22:54:49 -0800

    added text to speech client

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,12 +8,19 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    google()
+}
+allprojects {
+    repositories {
+        mavenCentral()
+        google()
+    }
 }
 
 dependencies {
     //Google Cloud APIS
     implementation("com.google.cloud:google-cloud-storage:2.29.1")
-    implementation("com.google.mlkit:translate:17.0.2")
+    implementation("com.google.cloud:google-cloud-translate:2.30.0")
     implementation("com.google.cloud:google-cloud-texttospeech:2.31.0")
     //Retrofit for API call
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
--- src/main/kotlin/TextToSpeechClient.kt
+++ src/main/kotlin/TextToSpeechClient.kt
@@ -1,4 +1,22 @@
 package com.openphonics.data
 
-class TextToSpeechClient {
+import com.google.cloud.texttospeech.v1.*
+import com.google.cloud.texttospeech.v1.TextToSpeechClient
+import java.io.FileOutputStream
+
+class TextToSpeechClient(
+    private val language: String = "en-US",
+    private val gender: SsmlVoiceGender = SsmlVoiceGender.NEUTRAL,
+    private val textToSpeech: TextToSpeechClient = TextToSpeechClient.create(),
+    private val audioConfig: AudioConfig = AudioConfig.newBuilder().setAudioEncoding(AudioEncoding.MP3).build(),
+    private val voice: VoiceSelectionParams = VoiceSelectionParams.newBuilder().setLanguageCode(language).setSsmlGender(gender).build()) {
+    fun speech(text: String, audioLocation: String): String {
+        val input = SynthesisInput.newBuilder().setText(text).build()
+        val response = textToSpeech.synthesizeSpeech(input, voice, audioConfig);
+        val audioContents = response.audioContent
+        val out = FileOutputStream(audioLocation).buffered()
+        out.write(audioContents.toByteArray())
+        out.close()
+        return audioLocation
+    }
 }
