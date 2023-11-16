3325711391af3c02437989de77d51f9e46ea812e
Advait Vedant <advait.suraj.vedant@gmail.com>
2023-11-15 18:30:31 -0800

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    application
+    kotlin("jvm") version "1.9.20"
+}
+
+group = "com.openphonics.data"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    //Google Cloud APIS
+    implementation("com.google.cloud:google-cloud-storage:2.29.1")
+    implementation("com.google.mlkit:translate:17.0.2")
+    implementation("com.google.cloud:google-cloud-texttospeech:2.31.0")
+    //Retrofit for API call
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //Testing
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
